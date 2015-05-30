f91fb491aaca9810cd1f3f0de50cb542915c4b64
unknown <serlokiju@gmail.com>
2015-05-29 21:03:31 -0300

    FUCK THAT modified:   index.php

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
-	phpinfo();
+	// phpinfo();
 	
+		
+	$connectionInfo = array( "Database"=>"fgt-db-dev",
+	 "UID"=>"lokiju", "PWD"=>"GoLang12345");
+
+	$conn = sqlsrv_connect( "r42x8255gz.database.windows.net,1433",
+				 $connectionInfo);
+	if ($conn < 0) {
+		echo "GO";
+	} else {
+		echo "NO GO";
+	}
 
 ?>
